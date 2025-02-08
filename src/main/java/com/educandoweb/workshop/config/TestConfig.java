4913dd9e5fde8a0e0f7157955174520d3d9393fd
Blazz <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2025-02-08 14:31:59 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/workshop/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.workshop.entities.Category;
 import com.educandoweb.workshop.entities.Order;
 import com.educandoweb.workshop.entities.OrderItem;
+import com.educandoweb.workshop.entities.Payment;
 import com.educandoweb.workshop.entities.Product;
 import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.entities.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2025-01-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
