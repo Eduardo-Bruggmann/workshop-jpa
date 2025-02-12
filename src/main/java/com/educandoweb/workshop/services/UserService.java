10ca85ec50d578a59edc4258d41437b4dce54715
Blazz <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2025-02-12 14:05:26 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/workshop/services/UserService.java
+++ src/main/java/com/educandoweb/workshop/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.workshop.repositories.UserRepository;
 import com.educandoweb.workshop.services.exceptions.DatabaseException;
 import com.educandoweb.workshop.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -33,9 +35,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public void delete(Long id) {
